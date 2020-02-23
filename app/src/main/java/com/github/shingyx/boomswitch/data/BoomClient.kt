3f896313c57e3402908928d66882027d823b1e1d
Su-Shing Chen <shingyx.dev@gmail.com>
2020-02-23 18:37:36 +1300

    Refactor Gatt code for more clarity
    Split the primary business logic into separate, documented functions and
    call them from the Gatt callback functions.

--- app/src/main/java/com/github/shingyx/boomswitch/data/BoomClient.kt
+++ app/src/main/java/com/github/shingyx/boomswitch/data/BoomClient.kt
@@ -17,9 +17,13 @@ private val SERVICE_UUID = UUID.fromString("000061fe-0000-1000-8000-00805f9b34fb
 private val WRITE_POWER_UUID = UUID.fromString("c6d6dc0d-07f5-47ef-9b59-630622b01fd3")
 private val READ_STATE_UUID = UUID.fromString("4356a21c-a599-4b94-a1c8-4b91fca02a9a")
 
+// READ_STATE_UUID values
 private const val BOOM_INACTIVE_STATE = 0.toByte()
+
+// WRITE_POWER_UUID values
 private const val BOOM_POWER_ON = 1.toByte()
-private const val BOOM_STANDBY = 2.toByte()
+private const val BOOM_POWER_OFF = 2.toByte()
+
 private const val TIMEOUT = 15000L
 
 object BoomClient {
@@ -123,6 +127,10 @@ private class BoomClientInternal(
         reject("Timed out in state $boomClientState", resId)
     }
 
+    /**
+     * Initiates connecting to the speaker's Gatt server.
+     * Called by entry function [switchPower] and continued by [onConnectionStateChange].
+     */
     private fun initializeConnection() {
         boomClientState = BoomClientState.CONNECTING
 
@@ -134,6 +142,68 @@ private class BoomClientInternal(
 
         gatt = device.connectGatt(context, false, gattCallback, BluetoothDevice.TRANSPORT_LE)
             ?: return reject("connectGatt returned null", R.string.error_null_bluetooth_client)
+    }
+
+    /**
+     * Scans for the available Gatt services and characteristics.
+     * Called by [onConnectionStateChange] and continued by [onServicesDiscovered].
+     */
+    private fun discoverGattServices() {
+        boomClientState = BoomClientState.DISCOVERING_SERVICES
+
+        if (!gatt.discoverServices()) {
+            reject("discoverServices failed", R.string.error_switching_power_failed)
+        }
+    }
+
+    /**
+     * Reads the speaker's current state.
+     * Called by [onServicesDiscovered] and continued by [onCharacteristicRead].
+     */
+    private fun readStateCharacteristic() {
+        boomClientState = BoomClientState.READING_STATE
+
+        val stateCharacteristic = gatt.getService(SERVICE_UUID)?.getCharacteristic(READ_STATE_UUID)
+        if (stateCharacteristic == null || !gatt.readCharacteristic(stateCharacteristic)) {
+            reject("readCharacteristic failed", R.string.error_switching_power_failed)
+        }
+    }
+
+    /**
+     * Writes the new power state for the speaker to toggle it on or off.
+     * Called by [onCharacteristicRead] and continued by [onCharacteristicWrite].
+     */
+    private fun writePowerCharacteristic(speakerIsInactive: Boolean) {
+        boomClientState = BoomClientState.WRITING_POWER
+
+        val powerByteValue: Byte
+
+        if (speakerIsInactive) {
+            switchingOn = true
+            powerByteValue = BOOM_POWER_ON
+        } else {
+            switchingOn = false
+            powerByteValue = BOOM_POWER_OFF
+        }
+
+        val powerCharacteristic = gatt.getService(SERVICE_UUID)?.getCharacteristic(WRITE_POWER_UUID)
+        if (powerCharacteristic != null) {
+            powerCharacteristic.value = byteArrayOf(0, 0, 0, 0, 0, 0, powerByteValue)
+            if (gatt.writeCharacteristic(powerCharacteristic)) {
+                return
+            }
+        }
+
+        reject("writeCharacteristic failed", R.string.error_switching_power_failed)
+    }
+
+    /**
+     * Closes the Gatt connection to the speaker.
+     * Called by [onCharacteristicWrite] and continued by [onConnectionStateChange].
+     */
+    private fun disconnectSpeaker() {
+        boomClientState = BoomClientState.DISCONNECTING
+        gatt.disconnect()
     }
 
     override fun onConnectionStateChange(status: Int, newState: Int) {
@@ -152,10 +222,7 @@ private class BoomClientInternal(
             return
         }
 
-        boomClientState = BoomClientState.DISCOVERING_SERVICES
-        if (!gatt.discoverServices()) {
-            reject("discoverServices failed", R.string.error_switching_power_failed)
-        }
+        discoverGattServices()
     }
 
     override fun onServicesDiscovered(status: Int) {
@@ -164,11 +231,7 @@ private class BoomClientInternal(
             return
         }
 
-        boomClientState = BoomClientState.READING_STATE
-        val stateCharacteristic = gatt.getService(SERVICE_UUID)?.getCharacteristic(READ_STATE_UUID)
-        if (stateCharacteristic == null || !gatt.readCharacteristic(stateCharacteristic)) {
-            reject("readCharacteristic failed", R.string.error_switching_power_failed)
-        }
+        readStateCharacteristic()
     }
 
     override fun onCharacteristicRead(characteristic: BluetoothGattCharacteristic, status: Int) {
@@ -177,23 +240,8 @@ private class BoomClientInternal(
             return
         }
 
-        boomClientState = BoomClientState.WRITING_POWER
-        val powerCharacteristic = gatt.getService(SERVICE_UUID)?.getCharacteristic(WRITE_POWER_UUID)
-        if (powerCharacteristic != null) {
-            val message = ByteArray(7)
-            switchingOn = characteristic.value[0] == BOOM_INACTIVE_STATE
-            message[6] = if (switchingOn) {
-                BOOM_POWER_ON
-            } else {
-                BOOM_STANDBY
-            }
-            powerCharacteristic.value = message
-            if (gatt.writeCharacteristic(powerCharacteristic)) {
-                return
-            }
-        }
-
-        reject("writeCharacteristic failed", R.string.error_switching_power_failed)
+        val speakerIsInactive = characteristic.value[0] == BOOM_INACTIVE_STATE
+        writePowerCharacteristic(speakerIsInactive)
     }
 
     override fun onCharacteristicWrite(characteristic: BluetoothGattCharacteristic, status: Int) {
@@ -202,7 +250,6 @@ private class BoomClientInternal(
             return
         }
 
-        boomClientState = BoomClientState.DISCONNECTING
-        gatt.disconnect()
+        disconnectSpeaker()
     }
 }
