ac1d1f011380e5249064ba0f0797535b4bded914
Su-Shing Chen <shingyx.dev@gmail.com>
2025-08-24 13:21:58 +1000

    Kotlin dsl (#29)
    * plugins blocks
    
    * kts
    
    * version catalog

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.util.Properties
+
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.oss.licenses.plugin)
+  alias(libs.plugins.ktfmt)
+  alias(libs.plugins.kotlin.android)
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+
+try {
+  keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+} catch (_: FileNotFoundException) {
+  logger.warn("keystore.properties not found")
+}
+
+android {
+  namespace = "com.github.shingyx.boomswitch"
+  signingConfigs {
+    create("release") {
+      storeFile = rootProject.file(keystoreProperties.getProperty("storeFile") ?: "default.jks")
+      storePassword = keystoreProperties.getProperty("storePassword")
+      keyAlias = keystoreProperties.getProperty("keyAlias")
+      keyPassword = keystoreProperties.getProperty("keyPassword")
+    }
+  }
+  compileSdk = 36
+  defaultConfig {
+    applicationId = "com.github.shingyx.boomswitch"
+    minSdk = 23
+    targetSdk = 36
+    versionCode = 13
+    versionName = "1.9"
+  }
+  buildTypes {
+    getByName("debug") {
+      applicationIdSuffix = ".dev"
+      versionNameSuffix = "-dev"
+    }
+    getByName("release") {
+      isMinifyEnabled = true
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+      signingConfig = signingConfigs["release"]
+    }
+  }
+  buildFeatures {
+    buildConfig = true
+    viewBinding = true
+  }
+  kotlin { jvmToolchain(21) }
+}
+
+ktfmt { googleStyle() }
+
+dependencies {
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.kotlinx.coroutines.android)
+  implementation(libs.material)
+  implementation(libs.oss.licenses)
+  implementation(libs.timber)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ktfmt) apply false
+    alias(libs.plugins.oss.licenses.plugin) apply false
+}
